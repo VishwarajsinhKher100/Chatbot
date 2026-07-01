deb41560c64e8ff41c7ad583e8095bb970f54494
Vishwarajsinh Kher <vishwarajsinh.kher100@gmail.com>
2026-07-01 23:07:11 +0530

    initial commit

---
+++ app/langgraph_backend.py
@@ -0,0 +1,31 @@
+from typing import TypedDict, Annotated
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import BaseMessage
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import InMemorySaver
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = init_chat_model("groq:llama-3.1-8b-instant")
+
+# State
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+   messages = state["messages"]
+   response = model.invoke(messages)
+   return {"messages": [response]}
+
+# Checkpointer
+checkpointer = InMemorySaver()
+
+# Graph
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
