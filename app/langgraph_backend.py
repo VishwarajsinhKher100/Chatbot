0e7a8abdb9148c4f23820bc3f8391de474d35e42
Vishwarajsinh Kher <vishwarajsinh.kher100@gmail.com>
2026-07-02 16:40:10 +0530

    feat(backend): add SQLite database for persistent chat history

--- app/langgraph_backend.py
+++ app/langgraph_backend.py
@@ -3,8 +3,9 @@ from langchain.chat_models import init_chat_model
 from langchain_core.messages import BaseMessage
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from dotenv import load_dotenv
+import sqlite3
 
 load_dotenv()
 
@@ -20,8 +21,9 @@ def chat_node(state: ChatState):
    response = model.invoke(messages)
    return {"messages": [response]}
 
+conn = sqlite3.connect(database="chatbot.db", check_same_thread=False)
 # Checkpointer
-checkpointer = InMemorySaver()
+checkpointer = SqliteSaver(conn=conn)
 
 # Graph
 graph = StateGraph(ChatState)
@@ -30,3 +32,10 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config["configurable"]["thread_id"])
+    
+    return list(all_threads)
