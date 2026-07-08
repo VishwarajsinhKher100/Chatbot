2ee17db3481ddcd1b7c05c147527872b5a801994
Vishwarajsinh Kher <vishwarajsinh.kher100@gmail.com>
2026-07-09 00:03:18 +0530

    feat(tool): add weather tool

--- app/langgraph_backend.py
+++ app/langgraph_backend.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_core.messages import BaseMessage
 from langchain.chat_models import init_chat_model
 from langgraph.graph.message import add_messages
 from langgraph.checkpoint.sqlite import SqliteSaver
@@ -44,6 +44,36 @@ def calculator(first_num: float, second_num: float, operation: str) -> dict:
         return {"error": str(e)}
 
 @tool
+def weather(city: str) -> dict:
+    """
+    Fetch the current weather (temperature, humidity, and wind speed) for a given city.
+    Only provide the city name as the input. ALWAYS use this tool for weather requests.
+    """
+
+    HEADERS={"User-Agent": "MCP-Crash-Course"}
+    geo = requests.get("https://nominatim.openstreetmap.org/search",
+                       params={"q": city, "format": "json", "limit": 1},
+                       headers=HEADERS).json()
+    
+    if not geo:
+        return "city not found"
+    
+    lat = geo[0]["lat"]
+    lon = geo[0]["lon"]
+
+    weather = requests.get("https://api.open-meteo.com/v1/forecast",
+                           params={
+                                "latitude": lat,
+	                            "longitude": lon,
+	                            "current": "temperature_2m,relative_humidity_2m,wind_speed_10m"}).json()["current"]
+    
+    return {
+        "City": city.title(),
+        "Temperature": f"{weather['temperature_2m']} C",
+        "Humidity": f"{weather['relative_humidity_2m']} %",
+        "Wind Speed": f"{weather['wind_speed_10m']} km/h"}
+
+@tool
 def get_stock_price(symbol: str) -> dict:
     """
     Fetch latest stock price for a given symbol (e.g. "AAPL", "TSLA")
@@ -53,7 +83,7 @@ def get_stock_price(symbol: str) -> dict:
     r = requests.get(url)
     return r.json()
 
-tools = [search_tool, get_stock_price, calculator]
+tools = [search_tool, get_stock_price, calculator, weather]
 model_with_tools = model.bind_tools(tools)
 
 # state
@@ -64,7 +94,7 @@ class ChatState(TypedDict):
 def chat_node(state: ChatState):
     """LLM node that may answer or request a tool call."""
     messages = state['messages']
-    response = model.invoke(messages)
+    response = model_with_tools.invoke(messages)
     return {"messages": [response]}
 
 tool_node = ToolNode(tools)
